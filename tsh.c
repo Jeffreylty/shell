746d40d644b9791d47064a7a743a8fd456a63ce1
Jeffreylty <38513016+Jeffreylty@users.noreply.github.com>
2019-04-13 23:19:48 -0400

    Update tsh.c

--- tsh.c
+++ tsh.c
@@ -283,6 +283,16 @@ int builtin_cmd(char **argv)
  */
 void do_bgfg(char **argv) 
 {
+
+
+
+	if(argv[1]==NULL){
+		printf("%s command requires PID or %%jobid argument\n", argv[0]);
+	}else{
+		
+	}
+
+	
     return;
 }
 
